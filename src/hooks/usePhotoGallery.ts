4e85dd00de96c6907392b4bd9341ddcb86385ef9
André Sousa <andrels9283@gmail.com>
2022-07-23 15:41:18 +0100

    feat: loading photos done

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -12,8 +12,26 @@ import { Storage } from '@capacitor/storage';
 import { Capacitor } from '@capacitor/core';
 import { UserPhoto } from '../models/UserPhoto/UserPhoto';
 
+const PHOTO_STORAGE = 'photos';
 export function usePhotoGallery() {
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const { value } = await Storage.get({ key: PHOTO_STORAGE });
+      const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
+
+      for (let photo of photosInStorage) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photosInStorage);
+    };
+    loadSaved();
+  }, []);
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -23,18 +41,48 @@ export function usePhotoGallery() {
     });
 
     const fileName = new Date().getTime() + '.jpeg';
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+    Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
+  };
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
   };
 
   return {
     photos,
     takePhoto,
   };
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+  const response = await fetch(path);
+  const blob = await response.blob();
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject('method did not return a string');
+      }
+    };
+    reader.readAsDataURL(blob);
+  });
 }
